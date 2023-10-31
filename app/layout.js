a405c17f05f1384f033ef88f77fbda2de079755e
Haoran-ryan <90777296+Haoran-ryan@users.noreply.github.com>
2023-11-01 09:51:51 +1100

    navbar structure created

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Nunito } from 'next/font/google'
 import './globals.css'
 
+import Navbar from "@/app/components/narbar/Navbar";
 const font = Nunito({ subsets: ['latin'] })
 
 export const metadata = {
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   )
 }
