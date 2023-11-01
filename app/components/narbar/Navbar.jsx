ba77fc475c8b6c58516dd90066e48381cec71b1b
Haoran-ryan <90777296+Haoran-ryan@users.noreply.github.com>
2023-11-01 14:25:47 +1100

    logo component, search component inistialised

--- app/components/narbar/Navbar.jsx
+++ app/components/narbar/Navbar.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
-
+import Container from "@/app/components/Container";
+import Logo from "@/app/components/narbar/Logo";
+import Search from "@/app/components/narbar/Search";
 const Navbar = () => {
     return (
-        <div>
-            Nav bar is coming 
+        <div className='fixed w-full bg-white z-10 shadow-sm'>
+            <div
+                className='
+                    py-4
+                    border-b-[1px]
+                '
+            >
+                <Container>
+                    <div className="
+                        flex
+                        flex-row
+                        items-center
+                        justify-between
+                        gap-3
+                        md:gap-0
+                    ">
+
+                    </div>
+                    <Logo />
+                    <Search />
+
+                    Nav bar is coming
+                </Container>
+
+            </div>
         </div>
     );
 };
